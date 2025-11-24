7e1ffcfc2d3d128b7141b14ae9b11ed595edd12d
Charlélise <charlelise@hotmail.fr>
2025-11-24 11:21:09 +0100

    fix: auth secret missing

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,10 +2,12 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
+  session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 } satisfies NextAuthOptions;
