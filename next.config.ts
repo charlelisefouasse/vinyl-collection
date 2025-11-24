0223b0aa0d038b83500be55a3834d2823147e611
Charlélise <charlelise@hotmail.fr>
2025-11-24 15:12:32 +0100

    feat: add secondary image source

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://i.scdn.co/image/**")],
+    remotePatterns: [
+      new URL("https://i.scdn.co/image/**"),
+      {
+        protocol: "https",
+        hostname: "atthemoviesshop.com",
+        pathname: "/cdn/shop/files/**",
+      },
+    ],
   },
 };
 
