56c69ff8155cdabf962843f271fb88fc910cae95
Charlélise <charlelise@hotmail.fr>
2026-02-19 11:34:19 +0100

    fix: prefetch vinyls list

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import { getCollectionQueryOptions } from "@/services/albums/service";
 import VinylsPage from "@/app/vinyls_page";
+import prisma from "@/lib/prisma";
 
 export default async function Home() {
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(getCollectionQueryOptions());
+
+  const vinyls = await prisma.album.findMany({
+    orderBy: [{ artist: "asc" }, { release_date: "asc" }],
+  });
+
+  queryClient.setQueryData(["vinyls"], vinyls);
+
   const dehydratedState = dehydrate(queryClient);
 
   return (
